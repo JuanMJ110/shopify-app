0c950c3cb94d524a95c0885e50c88e2b3ec7bc45
Juan José M <juan.moreno.jaramillo@correounivalle.edu.co>
2025-10-10 11:22:57 -0500

    Test variables de entorno

--- app/routes/webhooks.inventory.jsx
+++ app/routes/webhooks.inventory.jsx
@@ -84,7 +84,7 @@ async function getInventoryItemDetails(admin, inventoryItemId) {
 
 async function syncWithShipeu({ sellerId, operation, data }) {
   const url = process.env.SHIPEU_URL || 'http://dev.shipeu.com/api/shopify';
-  const apiKey = process.env.SHIPEU_API_KEY || '08afb311-1009-45a9-923e-0c032a4676e2';
+  const apiKey = process.env.SHIPEU_API_KEY;
   return fetch(`${url}/store/inventory`, {
     method: 'POST',
     headers: {
